88d69f574bf217ac84d79f516777cbc3aadc7523
Sura <suri.murodova@gmail.com>
2024-07-22 13:10:05 +0300

    Add docstring

--- app/library/lib_user_operations.py
+++ app/library/lib_user_operations.py
@@ -4,6 +4,12 @@ from .lib_methods import Library
 class UserOperation:
     @staticmethod
     def insert_new_book(lib: Library) -> str:
+        """
+        Добавляет новую книгу в библиотеку через пользовательский ввод.
+
+        :param lib: Экземпляр класса Library
+        :return: Строка потдверждения добавления книги или ошибка
+        """
         print("Введите следующие данные для добавления книги:")
         title = input("1. Название книги: ")
         author = input("2. Автор книги: ")
@@ -20,18 +26,27 @@ class UserOperation:
 
     @staticmethod
     def remove_book(lib: Library) -> str:
+        """
+        Удаляет книгу из библиотеки через пользовательский ввод.
+
+        :param lib: Экземпляр класса Library
+        :return: Строка потдверждения удаления книги или ошибка
+        """
         book_id = input("Введите id книги, которую нужно удалить: ").split(":")[-1]
         try:
             book_id = int(book_id)
         except ValueError:
             return "Некорректный id книги."
-        if book_id:
-            return lib.delete_book(book_id)
-        else:
-            return "Некорректный id книги."
+        return lib.delete_book(book_id)
 
     @staticmethod
     def find_book(lib: Library) -> list | str:
+        """
+        Находит книги в библиотеке через пользовательский ввод.
+
+        :param lib: Экземпляр класса Library
+        :return: Список найденных книг или сообщение об отсутствии книг / о некорректных данных
+        """
         book_item = input("Введите название/автора/год издания книги, которую нужно найти: ").split(":")[-1]
         if book_item:
             if book_item.isdigit():
@@ -44,10 +59,22 @@ class UserOperation:
 
     @staticmethod
     def all_books(lib: Library) -> list | str:
+        """
+        Отображает все книги в библиотеке.
+
+        :param lib: Экземпляр класса Library
+        :return: Список всех книг или сообщение об отсутствии книг
+        """
         return lib.display_all_books()
 
     @staticmethod
     def update_status(lib: Library) -> str:
+        """
+        Обновляет статус книги через пользовательский ввод.
+
+        :param lib: Экземпляр класса Library
+        :return: Строка подтверждения изменения статуса или ошибка
+        """
         book_id = input("Введите id книги, у которой хотите поменять статус: ").split(":")[-1]
         try:
             book_id = int(book_id)
