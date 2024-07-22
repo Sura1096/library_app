317c89616793f0f40ac76010c313b5e187af669e
Sura <suri.murodova@gmail.com>
2024-07-22 13:09:18 +0300

    Add docstring

--- app/file_handle/file_handle.py
+++ app/file_handle/file_handle.py
@@ -3,9 +3,19 @@ import json
 
 class FileHandler:
     def __init__(self, file_name):
+        """
+        Инициализирует обрабработчик файла.
+
+        :param file_name: Имя файла
+        """
         self.file_name = file_name
 
     def read_file_content(self) -> dict:
+        """
+        Читает содержимое файла и возвращает данные в виде словаря.
+
+        :return: Данные из файла в виде словаря
+        """
         try:
             with open(self.file_name, 'r', encoding='utf-8') as file:
                 books = json.load(file)
@@ -14,5 +24,11 @@ class FileHandler:
         return books
 
     def add_new_content(self, data: dict) -> None:
+        """
+        Записывает новые данные в файл.
+
+        :param data: Данные для записи в файл
+        :return: None
+        """
         with open(self.file_name, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
